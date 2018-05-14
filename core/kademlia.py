cd4771ad3def8a162a45e01a63fed3d4e40aac6c
dashm8 <tgamer553@gmail.com>
2018-05-14 11:47:11 -0700

    update

--- core/kademlia.py
+++ core/kademlia.py
@@ -1,27 +1,25 @@
 # this is gonna be ruff
 import hashlib
 
-
+'''
+        self.clients = clients#dict {id:(conn)}
+        self.id = id
+        self.ip = ip
+        self.port = port
+        self.router = router#callable function
+'''
 class Peer:
-    def __init__(self, kmax, username):
+    def __init__(self, kmax, client):
         '''
-
         :param kmax: the (most of the time) fixed size of the maximum amount of peers that one peer can hold.
         :param username: simply the peer's username, will be used in the generation of the peer's id.
         '''
-        self.id = hashlib.sha256(username)
-        self.RoutingTable = {} # this dictionary contains the peers that this peer is connected to and their range from this peer, the format is {"hashed_peer_id":(hashed_peer_id,range from this peer)}
+        self.client = client
+        self.id = hashlib.sha256(client.id).hexdigest()
+        self.RoutingTable = {} # this dictionary contains the peers that this peer is connected to and their range from this peer, the format is {"hashed_peer_id":((ip,port)),range from this peer)}
         #self.RoutingTable = {}  # this dictionary contains the peers who are this peer is connected to, the format is {"hashed_peer_id":hashed_peer_id}
         self.kmax = kmax
-        self.k = 0
-
-    def get_k(self):
-        '''
-
-        :return: the number of nodes this client currently has.
-        '''
-        # return len(self.RoutingTable)
-        return self.k
+        self.k = 0        
 
     def get_range(self, peer_id):
         '''
@@ -33,19 +31,28 @@ class Peer:
         peeridb = int(peer_id, 16)
         return my_id ^ peeridb
 
-    def add_peer(self, peer_username):  # might be add_friend
+    def add_peer(self, peer_username,endpoint):  # might be add_friend
         '''
 
         :param peer_username: the new peer's username
         :return: this function doesn't return anything.
         this function adds a new peer to the tree, is the base "Sign up" function
-        '''
-        peer_id = hashlib.sha256(peer_username)
-        self.RoutingTable["peer_"+str(self.k)] =
-        {"peer_id"}
+        '''        
+        peer_id = hashlib.sha256(peer_username).hexdigest()
+        self.RoutingTable[peer_id] = (endpoint,self.get_range(peer_id))
+        self.k += 1
+
+    def bootstrap(self):        
+        
 
 
 class Router:
+    def __init__(self):
+        self.peer = Peer()
+
+
+
+class RouterProtocol:
     print("do stuff")
 
 
@@ -56,3 +63,12 @@ class Utils:
         peerida = int(peerida, 16)
         peeridb = int(peeridb, 16)
         return peerida ^ peeridb
+'''
+death signal -> temp table
+ping
+store
+add
+divide
+updatek
+bootstrap
+'''
